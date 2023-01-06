6bfb186bfca7bfd6947dbd01bdd96441f6fd0ebc
sma11case <udf.q@qq.com>
2023-01-06 14:36:40 +0800

    v1

--- misc/setup.sh
+++ misc/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sdir=`realpath "$0"`
-sdir=`dirname "$sdir"`
+sdir=`dirname "$0"`
+sdir=`realpath "$sdir/.."`
+
 
 yum install -y epel-release
 yum install -y zsh rsync vim screen iftop iotop htop git unzip
@@ -12,7 +13,6 @@ git clone 'https://e.coding.net/sma11case/demo1/zsh-demo.git'
 ln -sf /data1/zsh-demo/.oh-my-zsh "$HOME/"
 ln -sf /data1/zsh-demo/.zshrc "$HOME/"
 
-cd "$sdir"
 ln -sf "$sdir/config/.screenrc" "$HOME/"
 ln -sf "$sdir/config/config/.vimrc" "$HOME/"
 
