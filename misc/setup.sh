415920172ef885eb48b29a220004243057c45660
sma11case <udf.q@qq.com>
2023-01-06 14:38:36 +0800

    v1

--- misc/setup.sh
+++ misc/setup.sh
@@ -7,7 +7,7 @@ sdir=`realpath "$sdir/.."`
 yum install -y epel-release
 yum install -y zsh rsync vim screen iftop iotop htop git unzip
 
-mkdir /data1
+[ -d '/data1' ] || mkdir -pv /data1
 cd /data1
 git clone 'https://e.coding.net/sma11case/demo1/zsh-demo.git'
 ln -sf /data1/zsh-demo/.oh-my-zsh "$HOME/"
