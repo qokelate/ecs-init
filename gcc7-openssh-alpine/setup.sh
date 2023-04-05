06f08335b98e843d8556abc5f179455dedddfe2c
sma11case <udf.q@qq.com>
2023-04-05 13:19:51 +0800

    v1

--- gcc7-openssh-alpine/setup.sh
+++ gcc7-openssh-alpine/setup.sh
@@ -18,12 +18,17 @@ ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
 ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ''
 ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
 
+ninja=`which ninja|head -n 1`
+ln -sf "$ninja" "$ninja-build"
+
 # passwd -l root
 ssh-keygen -t ed25519 -f '/root/.ssh/id_ed25519' -q -N ""
 [ -n "$SSH_PUB_KEY" ] && echo "$SSH_PUB_KEY" > '/root/.ssh/authorized_keys'
 
-ninja=`which ninja|head -n 1`
-ln -sf "$ninja" "$ninja-build"
+if [ -d "/data1/zsh-demo/.oh-my-zsh" ]; then
+    ln -sf "/data1/zsh-demo/.zshrc" "$HOME/"
+    ln -sf "/data1/zsh-demo/.oh-my-zsh" "$HOME/"
+fi
 
 touch "$file"
 exit
