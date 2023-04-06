884262a56a848be680c8086b287b3e07527f91d4
sma11case <udf.q@qq.com>
2023-04-06 10:24:43 +0800

    v1

--- misc/rhel9.sh
+++ misc/rhel9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf install 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm'
+dnf install -y 'https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm'
 
 dnf install -y net-tools zsh git rsync screen htop iftop iotop
 
