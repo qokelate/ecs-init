d1b06e1ee994a33eac61fd01d110359ebaf35f91
sma11case <udf.q@qq.com>
2023-04-06 11:59:00 +0800

    v1

--- misc/setup.sh
+++ misc/setup.sh
@@ -9,7 +9,7 @@ yum install -y zsh rsync vim screen iftop iotop htop git unzip
 
 [ -d '/data1' ] || mkdir -pv /data1
 cd /data1
-git clone 'https://e.coding.net/sma11case/demo1/zsh-demo.git'
+git clone 'https://e.coding.net/smallcase/demo1/zsh-demo.git'
 ln -sf /data1/zsh-demo/.oh-my-zsh "$HOME/"
 ln -sf /data1/zsh-demo/.zshrc "$HOME/"
 
