9c16d7ee00e9fe154d899b55a6124e4dc2a214ab
sma11case <udf.q@qq.com>
2023-12-02 09:18:08 +0800

    v1

---
+++ ubuntu/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt install -y language-pack-zh-hans
+apt install -y zsh rsync vim screen iftop iotop htop git unzip bind-utils net-tools zip
+
+mkdir -pv /data1; cd /data1
+git clone 'https://e.coding.net/smallcase/demo1/zsh-demo.git'
+ln -sf /data1/zsh-demo/.oh-my-zsh "$HOME/"
+ln -sf /data1/zsh-demo/.zshrc "$HOME/"
+
+ln -sf "$sdir/config/.screenrc" "$HOME/"
+ln -sf "$sdir/config/.vimrc" "$HOME/"
+
+exit
