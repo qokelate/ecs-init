7a102ad07cf58548299cc5bb86cc20e21f428eff
sma11case <udf.q@qq.com>
2025-07-18 09:48:50 +0800

    v1

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -2,15 +2,15 @@
 
 apt update
 apt install -y language-pack-zh-hans
-apt install -y zsh rsync vim screen iftop iotop htop git unzip net-tools zip
+apt install -y dnsutils iputils-ping telnet zsh rsync vim screen iftop iotop htop git unzip net-tools zip
 
 mkdir -pv /data1; cd /data1
 git clone 'https://e.coding.net/smallcase/demo1/zsh-demo.git'
 ln -sf /data1/zsh-demo/.oh-my-zsh "$HOME/"
 ln -sf /data1/zsh-demo/.zshrc "$HOME/"
 
-sdir=`dirname "$0"`
-sdir=`realpath "$sdir/.."`
+sdir=`git rev-parse --show-toplevel`
+sdir=`realpath "$sdir"`
 ln -sf "$sdir/config/.screenrc" "$HOME/"
 ln -sf "$sdir/config/.vimrc" "$HOME/"
 
